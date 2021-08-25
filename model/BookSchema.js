64796df65b626a4b1364114fe746f2434fed9303
shamikhquraan1 <shamikhq@yahoo.com>
2021-08-25 15:07:56 +0300

    adding bkend lab013

---
+++ model/BookSchema.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+const mongoose = require("mongoose");
+
+const BookSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  status: String,
+  email: String,
+})
+
+module.exports = BookSchema; 
