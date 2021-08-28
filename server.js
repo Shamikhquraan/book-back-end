fdac109212ededc8e1dc28bf4c2e4a7f1271e620
shamikhquraan1 <shamikhq@yahoo.com>
2021-08-28 13:33:44 +0300

    adding

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 "use strict";
 
-require("dotenv").config();
-const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
-
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const server = express();
 const PORT = process.env.PORT;
 server.use(cors());
-const axios = require("axios");
 server.use(express.json());
 
 // const mongoVar= process.env.MONGO_LINK  , put the a .... and dasdsad loc;
@@ -20,8 +17,7 @@ mongoose.connect(`${process.env.DB_LINK}`, {
 
 const BookSchema = require("./model/BookSchema.js");
 
-
-const BookModel = mongoose.model("books",BookSchema);
+const BookModel = mongoose.model("books", BookSchema);
 
 // function seedDataCollection() {
 //   const book1 = new BookModel({
@@ -67,17 +63,16 @@ const BookModel = mongoose.model("books",BookSchema);
 server.get("/", testHandler);
 server.get("/getBooks", getBooksHandler);
 server.post("/addBook", addBookHandler);
-server.delete('/deletBook/:bookId', deletBookHandler);
-server.put('/updateBook/:bookId', updateBookHandler);
-
+server.delete("/deletBook/:bookId", deletBookHandler);
+server.put("/updateBook/:bookId", updateBookHandler);
 
 function testHandler(req, res) {
   res.send("test");
-};
+}
 
 // localhost:3001/getBooks?emailName=dark.lord1122@outlook.sa
 
- function getBooksHandler(req, res) {
+function getBooksHandler(req, res) {
   console.log("inside getCatsHandler func");
   let emailName2 = req.query.email;
   BookModel.find({ email: emailName2 }, function (err, ownerData) {
@@ -85,10 +80,10 @@ function testHandler(req, res) {
       console.log("error in getting the data");
     } else {
       console.log(ownerData);
-       res.send(ownerData);
+      res.send(ownerData);
     }
-  })
-};
+  });
+}
 
 // localhost:3002/addBook,{ title: "shamikh",  description: "",  status: "On Stock",  email: "dark.lord1122@outlook.sa" }
 async function addBookHandler(req, res) {
@@ -107,56 +102,55 @@ async function addBookHandler(req, res) {
   getBooksHandler(req, res); // send data tso frontEnd......
 }
 
+async function deletBookHandler(req, res) {
+  console.log("inside deletBookHandler func");
 
-
-async function deletBookHandler(req,res) {
-  console.log('inside deletBookHandler func');
-
-  let emailOwner= req.query.email;
+  let emailOwner = req.query.email;
 
   let bookId = req.params.bookId;
-  BookModel.remove({_id:bookId},(error,deletedBook)=>{
-      if(error) {
-          console.log('error in deleteing the data')
-      } else {
-          console.log('data deleted', deletedBook)
-          BookModel.find({email: emailOwner},function(err,data){
-              if(err) {
-                  console.log('error in getting the data')
-              } else {
-                  console.log(data);
-                  res.send(data);
-              }
-          })
-      }
-  })
-
+  BookModel.remove({ _id: bookId }, (error, deletedBook) => {
+    if (error) {
+      console.log("error in deleteing the data");
+    } else {
+      console.log("data deleted", deletedBook);
+      BookModel.find({ email: emailOwner }, function (err, data) {
+        if (err) {
+          console.log("error in getting the data");
+        } else {
+          console.log(data);
+          res.send(data);
+        }
+      });
+    }
+  });
 }
 
-
-
-
-async function updateBookHandler(req,res) { 
-  console.log('inside updateBookHandler func');
+async function updateBookHandler(req, res) {
+  console.log("inside updateBookHandler func");
   let bookId = req.params.bookId;
-  let { title, description, status,email }=req.body;
+  let { title, description, status, email } = req.body;
   console.log(req.body);
-  BookModel.findByIdAndUpdate(bookId, { title , description, status, email },(error,updatedData)=>{//updatedDatais for onejhhggj obj just 
-      if(error) {
-          console.log('error in updating the data')
+  BookModel.findByIdAndUpdate(
+    bookId,
+    { title, description, status, email },
+    (error, updatedData) => {
+      //updatedDatais for onejhhggj obj just
+      if (error) {
+        console.log("error in updating the data");
       } else {
-          console.log(updatedData,"Data updated!");
-          
-          BookModel.find({email: req.body.email},function(err,data){
-              if(err) {
-                  console.log('error in getting the data')
-              } else {
-                console.log(data);
-                  res.send(data);
-              }
-          })
+        console.log(updatedData, "Data updated!");
+
+        BookModel.find({ email: req.body.email }, function (err, data) {
+          if (err) {
+            console.log("error in getting the data");
+          } else {
+            console.log(data);
+            res.send(data);
+          }
+        });
       }
-  })
+    }
+  );
 }
 
 server.listen(PORT, () => {
